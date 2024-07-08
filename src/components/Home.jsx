5a9fdf68c603f12f89abcee86db5090eb4640a95
rajipmahato68 <rajipmahato68@gmail.com>
2024-07-08 14:51:12 +0545

    simple change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -46,7 +46,7 @@ function Home() {
     <div name="Home" className="max-w-screen-2xl mx-auto px-4 md:px-20 my-20">
         <div className='flex flex-col md:flex-row'>
         <div className='md:w-1/2 mt-12 md:mt-24 space-y-2 order-2 md:order-1'>
-        <span className='text-xl'>Welcome In My feed</span>
+        {/* <span className='text-xl'>जय श्री राम 💖💖💖🤞</span> */}
         <div className='flex space-x-1 text-2xl md:text-4xl'>
         <h1>Hello, I'm a </h1>
         {/* <span className='text-red-700 font-bold'>Developer</span> */}
